fa055de08bc746664a692bfe3cbe7f5016a1d8f7
alexthecurator <edge.works.curator@gmail.com>
2022-07-31 02:14:28 +0300

    Ui tweaks

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,26 @@
 const Home = () => {
   return (
-    <div className="bg-hero w-screen h-full xl:h-screen container mx-auto">
-      <div className="w-full flex flex-col items-center justify-center">
-        <span className="w-full flex flex-row items-center justify-between">
+    <div className="bg-hero w-screen h-full xl:h-screen ">
+      <div className="w-full h-full px-24 container mx-auto flex flex-col items-center justify-center">
+        <span className="w-full flex flex-row items-center justify-center spcae-x-12">
           {/* Left */}
-          <span className="flex flex-col space-y-4">
-            <h1 className="font-bold text-xl">
-              HSKO donates <br /> <span>0.5% of every transaction</span> <br />{" "}
-              to charity.
+          <span className="w-full flex flex-col space-y-6">
+            <h1 className="text-white font-bold text-7xl capitalize">
+              HSKO donates <br />{" "}
+              <span className="text-red-600">0.5% of every transaction</span>{" "}
+              <br /> to charity.
             </h1>
-            <p className="text-white font-bold">
-              HSKO is a p2p community-driven, cryptocurrency on the Polygon
-              blockchain (MATIC).{" "}
-              <span className="text-slate-500 font-light">
-                HSKO will have a maximum supply of 1 billion tokens, with a 1%
-                txn fee used for development, marketing and charities.
-              </span>
-            </p>
-            <span className="flex flex-row space-x-4 justify-start">
+            <span className="w-full max-w-xl">
+              <p className="text-white font-bold">
+                HSKO is a p2p community-driven, cryptocurrency on the Polygon
+                blockchain (MATIC).{" "}
+                <span className="text-slate-400 font-light">
+                  HSKO will have a maximum supply of 1 billion tokens, with a 1%
+                  txn fee used for development, marketing and charities.
+                </span>
+              </p>
+            </span>
+            <span className="w-full max-w-md flex flex-row space-x-4 justify-start">
               <Button
                 style={"solid"}
                 text={"buy now"}
@@ -37,7 +40,7 @@ const Home = () => {
             </span>
           </span>
           {/* Right */}
-          <span>
+          <span className="w-full max-w-md">
             <img src={"/logo.svg"} alt={"husko hero image"} />
           </span>
         </span>
@@ -49,7 +52,7 @@ const Home = () => {
 const Button = ({ style, text, action, type }) => {
   return (
     <button
-      className={`flex w-full h-full flex-row items-center justify-center space-x-1 text-base 2xl:text-xl py-[7px] 2xl:py-[11px] capitalize duration-300 whitespace-nowrap ${
+      className={`flex w-full flex-row items-center justify-center space-x-1 text-lg 2xl:text-xl py-5 2xl:py-[11px] capitalize duration-300 whitespace-nowrap ${
         style === "solid"
           ? "bg-white text-black hover:bg-slate-100 focus:bg-slate-200"
           : style === "outline"
